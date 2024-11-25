f63ad8759f05a15f6517a4ca9efcdb1d56decec1
iLan Epstein <stuff@mentalfabrications.com>
2024-11-25 00:42:16 -0800

    Tabs -> spaces

--- src/lib/components/starBackground/star.svelte
+++ src/lib/components/starBackground/star.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import { cn } from '$lib/utils';
+  import { cn } from '$lib/utils';
 
-	export interface Props {
-		brightness: number;
-		x: number;
-		y: number;
-		size: number;
-	}
+  export interface Props {
+    brightness: number;
+    x: number;
+    y: number;
+    size: number;
+  }
 
-	let { brightness, x, y, size }: Props = $props();
+  let { brightness, x, y, size }: Props = $props();
 </script>
 
 <div
-	class={cn('absolute', 'rounded-full', 'bg-white transition-all')}
-	style={`left: ${x}%; top: ${y}%; width: ${size}px; height: ${size}px; opacity: ${brightness};`}
+  class={cn('absolute', 'rounded-full', 'bg-white transition-all')}
+  style={`left: ${x}%; top: ${y}%; width: ${size}px; height: ${size}px; opacity: ${brightness};`}
 ></div>
