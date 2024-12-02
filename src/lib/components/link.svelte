3e1d4d1735459417a391ebe9b1e240694aa413de
iLan Epstein <stuff@mentalfabrications.com>
2024-12-01 21:26:58 -0800

    Add Link component

---
+++ src/lib/components/link.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { cn } from '$lib/utils';
+  import type { HTMLAnchorAttributes } from 'svelte/elements';
+
+  type $$Props = HTMLAnchorAttributes;
+
+  interface Props extends $$Props {
+    className?: $$Props['class'];
+  }
+
+  const { children, className, ...restProps }: Props = $props();
+</script>
+
+<a
+  class={cn(
+    className,
+    'text-emerald-300',
+    'underline',
+    'hover:text-emerald-400',
+    'transition-colors',
+  )}
+  {...restProps}
+>
+  {@render children?.()}
+</a>
